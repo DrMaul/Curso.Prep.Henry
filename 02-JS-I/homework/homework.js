a48140d59bc23b6fecd489388cde0696be27ece9
DrMaul <agusfmartinez99@gmail.com>
2022-09-27 11:13:19 -0300

    Tarea JSI finalizada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -70,10 +70,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.lenght === str2.lenght){
-    return true;
-  }
-  else {return false;}
+  return str1.length === str2.length;
 }
 
 function menosQueNoventa(num) {
@@ -99,7 +96,7 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  var resto = x%y;
+  return x%y;
 }
 
 function esPar(num) {
@@ -164,9 +161,10 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero>0){return console.log("Es positivo");}
-  else if (numero < 0){return console.log("Es negativo");}
+  if(numero>0){return "Es positivo";}
+  else if (numero < 0){return "Es negativo";}
   else {return false;}
+
 }
 
 function agregarSimboloExclamacion(str) {
@@ -187,7 +185,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return console.log('Hola ${nombre}!');
+  return "Hola " + nombre + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -225,12 +223,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.lenght==1){
-    if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
-    return "Es Vocal";}
-    else {return "Dato incorrecto";}}
-  else {return "Dato incorrecto";}
   
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal";}
+  if(letra.length>1){ return "Dato incorrecto";}
+  return "Dato incorrecto";
+} 
 
 
 
